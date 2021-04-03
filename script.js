98151810049e43b52e7de15bf40de61b27fd09d6
ankitt01 <ankitrawat0102@gmail.com>
2021-04-03 11:29:06 +0530

    nav sidebar added

---
+++ script.js
@@ -0,0 +1,9 @@
+// const ham = document.querySelector('.ham');
+// const main = document.querySelector('main');
+// const nav = document.querySelector('nav');
+// const sidebar = document.querySelector('.nav-sidebar');
+// ham.addEventListener('click', () => {
+//     sidebar.classList.toggle('sidebar-active');
+//     main.classList.toggle('main-active');
+//     nav.classList.toggle('nav-active');
+// })
